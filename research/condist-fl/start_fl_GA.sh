f575de5807db814ea0550cb4210f75cc74770289
qqaazz800624 <qqaazz800624@gmail.com>
2024-09-06 11:09:19 +0800

    update gitignore and shell script for one new experiment

--- research/condist-fl/start_fl_GA.sh
+++ research/condist-fl/start_fl_GA.sh
@@ -3,7 +3,7 @@
 export PYTHONPATH=$PWD/src:$PYTHONPATH
 
 nvflare simulator \
-  -w $PWD/workspace_GA \
+  -w $PWD/workspace_GA_DSLoss \
   -c liver,spleen,pancreas,kidney \
   -gpu 4,5,6,7 \
   jobs/condist_GA
