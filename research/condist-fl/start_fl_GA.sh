416f36f6844d006dd72c4718176b77ae43f7f5a0
qqaazz800624 <qqaazz800624@gmail.com>
2024-09-06 15:30:25 +0800

    update shell script for mednext

--- research/condist-fl/start_fl_GA.sh
+++ research/condist-fl/start_fl_GA.sh
@@ -3,7 +3,7 @@
 export PYTHONPATH=$PWD/src:$PYTHONPATH
 
 nvflare simulator \
-  -w $PWD/workspace_GA_DSLoss \
+  -w $PWD/workspace_GA_mednext \
   -c liver,spleen,pancreas,kidney \
   -gpu 4,5,6,7 \
   jobs/condist_GA
