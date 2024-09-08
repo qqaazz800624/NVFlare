d9e99fa8d2ba1d326e398fe3e3186bcaa2b87a48
qqaazz800624 <qqaazz800624@gmail.com>
2024-09-08 08:28:40 +0800

    rerun mednext version_1

--- research/condist-fl/start_fl_GA.sh
+++ research/condist-fl/start_fl_GA.sh
@@ -3,7 +3,7 @@
 export PYTHONPATH=$PWD/src:$PYTHONPATH
 
 nvflare simulator \
-  -w $PWD/workspace_GA_mednext2 \
+  -w $PWD/workspace_GA_mednext \
   -c liver,spleen,pancreas,kidney \
-  -gpu 1,0,2,3 \
+  -gpu 4,5,6,7 \
   jobs/condist_GA
