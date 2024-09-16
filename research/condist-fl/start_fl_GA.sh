070184cfc8c76fef13e7258fb8ec7a784cdcad6e
qqaazz800624 <qqaazz800624@gmail.com>
2024-09-16 23:20:20 +0800

    adjust GPUs

--- research/condist-fl/start_fl_GA.sh
+++ research/condist-fl/start_fl_GA.sh
@@ -3,7 +3,7 @@
 export PYTHONPATH=$PWD/src:$PYTHONPATH
 
 nvflare simulator \
-  -w $PWD/workspace_GA_mednext \
+  -w $PWD/workspace_GA_test \
   -c liver,spleen,pancreas,kidney \
-  -gpu 1,0,2,3 \
+  -gpu 4,5,6,7 \
   jobs/condist_GA
