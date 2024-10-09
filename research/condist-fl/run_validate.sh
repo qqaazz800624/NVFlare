0e6a5fc5a91d8ad866cf3755e4645721abeaee92
qqaazz800624 <qqaazz800624@gmail.com>
2024-10-09 14:38:06 +0000

    add a shell for validation

---
+++ research/condist-fl/run_validate.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Set default CUDA device to use
+DEFAULT_CUDA_DEVICE="2"
+
+# Allow overriding the default CUDA device with a command-line argument
+CUDA_DEVICE=${1:-$DEFAULT_CUDA_DEVICE}
+
+# Set the CUDA device to use
+export CUDA_VISIBLE_DEVICES=$CUDA_DEVICE
+
+python run_validate.py -w workspace_GA_mednext -o cross_site_validate_mednext.json $@
