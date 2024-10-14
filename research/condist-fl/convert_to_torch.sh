743c27bbebf93e22bd8f3d62469ab59c264c7c64
qqaazz800624 <qqaazz800624@gmail.com>
2024-10-14 12:32:41 +0800

    rename

--- research/condist-fl/convert_to_torch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# For server checkpoint
-python convert_to_torchscript.py \
-  --config workspace/simulate_job/app_pancreas/config/config_task.json \
-  --weights workspace/simulate_job/app_pancreas/models/best_model.pt \
-  --app pancreas \
-  --output best_pancreas_model.pt
