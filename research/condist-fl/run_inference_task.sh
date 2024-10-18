85fe12d252da351da3befa1d077aa23b715a1769
qqaazz800624 <qqaazz800624@gmail.com>
2024-10-18 11:40:23 +0800

    add by-task inference scripts

---
+++ research/condist-fl/run_inference_task.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# set default CUDA device
+DEFAULT_CUDA_DEVICE="1"   # 0, 1, 2, 3
+
+# allow overriding the default CUDA device with a command-line argument
+CUDA_DEVICE=${2:-$DEFAULT_CUDA_DEVICE}
+
+export CUDA_VISIBLE_DEVICES=${CUDA_DEVICE}
+
+# set default directory for data
+DEFAULT_DATA_ROOT="Pancreas"  # Pancreas, Liver, KiTS19, Spleen
+
+# allow overriding the default data directory with a command-line argument
+DATA_ROOT=${3:-$DEFAULT_DATA_ROOT}
+
+# set default model name to evaluate
+DEFAULT_MODEL_NAME="pancreas"   # kidney, liver, pancreas, spleen, global
+
+# allow overriding the default model name with a command-line argument
+MODEL_NAME=${1:-$DEFAULT_MODEL_NAME}
+
+
+python run_infer_task.py \
+  --data_root /neodata/open_dataset/ConDistFL/data/${DATA_ROOT} \
+  --data_list /neodata/open_dataset/ConDistFL/data/${DATA_ROOT}/datalist.json \
+  --data_list_key testing \
+  --model best_${MODEL_NAME}_model.pt \
+  --output infer \
+  --model_type nnUNet \
+  --client_name "${MODEL_NAME}"
+
+python run_infer_task.py \
+  --data_root /neodata/open_dataset/ConDistFL/data/${DATA_ROOT} \
+  --data_list /neodata/open_dataset/ConDistFL/data/${DATA_ROOT}/datalist.json \
+  --data_list_key testing \
+  --model best_${MODEL_NAME}_model_GA.pt \
+  --output infer_GA \
+  --model_type nnUNet \
+  --client_name "${MODEL_NAME}"
