90d7dbc60b5851e8a708a44a8975dd66906822a8
qqaazz800624 <qqaazz800624@gmail.com>
2024-10-28 20:38:22 +0800

    update another experiment

--- research/condist-fl/start_fl_GA.sh
+++ research/condist-fl/start_fl_GA.sh
@@ -3,7 +3,7 @@
 export PYTHONPATH=$PWD/src:$PYTHONPATH
 
 nvflare simulator \
-  -w $PWD/workspace_GA_condist \
+  -w $PWD/workspace_GA_evidential \
   -c liver,spleen,pancreas,kidney \
-  -gpu 2,1,0,3 \
-  jobs/condist_GA
+  -gpu 3,0,1,2 \
+  jobs/condist_GA_evidential
