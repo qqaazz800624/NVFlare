1dd7ba8b74fbc09d928fd56d28d2ca08c8959448
qqaazz800624 <qqaazz800624@gmail.com>
2025-01-26 08:53:17 +0800

    update

--- research/condist-fl/test.py
+++ research/condist-fl/test.py
@@ -2,9 +2,6 @@
 
 import torch
 
-
-#%%
-
 ckpt_path = "/home/u/qqaazz800624/NVFlare/research/condist-fl/workspace_GA_mednext/simulate_job/app_server/best_FL_global_model.pt"
 
 state_dict = torch.load(ckpt_path, map_location='cpu')
